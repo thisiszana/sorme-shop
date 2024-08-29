1ef68279a123912cbbaf2b9eab392b235c96511c
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-08-29 13:21:22 +0330

    Create user model.

---
+++ models/UserSorme.js
@@ -0,0 +1,55 @@
+import { Schema, models, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    displayName: {
+      type: String,
+      default: "",
+    },
+    phoneNumber: {
+      type: String,
+      default: "",
+    },
+    address: {
+      type: String,
+      default: "",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "USER",
+    },
+    gender: {
+      type: String,
+      enum: ["man", "female", "etc"],
+      default: "etc",
+    },
+    orders: [{ type: Schema.Types.ObjectId, ref: "Order", default: [] }],
+    cart: {
+      items: [
+        {
+          productId: { type: Schema.Types.ObjectId, ref: "Products" },
+          quantity: { type: Number, default: 0 },
+        },
+      ],
+      selectedItems: [{ type: Schema.Types.ObjectId, ref: "Products" }],
+      totalProductsCount: { type: Number, default: 0 },
+      checkoutStatus: {
+        type: String,
+        enum: ["pending", "completed"],
+        default: "pending",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+export const UserSorme = models.UserSorme || model("UserSorme", userSchema);
