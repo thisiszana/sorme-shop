f257651808061aec352f459e4749a71101e68035
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-08-31 00:16:24 +0330

    Writing a function to get the user's token and verify it to create an access level. Creating user access levels in the program.

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,3 +1,9 @@
+import { getServerSession } from "@/utils/session";
+import { redirect } from "next/navigation";
+
 export default function AuthLayout({ children }) {
+  const session = getServerSession();
+
+  if (session) redirect("/profile/personal-information");
   return <main>{children}</main>;
 }
