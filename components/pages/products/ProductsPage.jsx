eb3b2a8ca3815fd56e665699069c16d130caf788
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-09-01 00:26:12 +0330

    Get data using useQuery. Merge the returned data (allProducts, filterProducts).

--- components/pages/products/ProductsPage.jsx
+++ components/pages/products/ProductsPage.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Loader from "@/components/shared/Loader";
-import { getAllProducts } from "@/services/queries";
+import { getAllProducts, getFilterProducts } from "@/services/queries";
 import { QUERY_KEY } from "@/services/queriesKey";
 import { useQuery } from "@tanstack/react-query";
 import FilterProducts from "./ui/FilterProducts";
@@ -18,7 +18,15 @@ export default function ProductsPage({ searchParams }) {
     refetchInterval: 10000,
   });
 
-  console.log(allProducts);
+  const { data: filteredProduct, isLoading: isLoadingFilered } = useQuery({
+    queryKey: [QUERY_KEY.filteredProducts, searchParams],
+    queryFn: () => getFilterProducts(searchParams),
+    gcTime: 0,
+    staleTime: 0,
+    refetchInterval: 10000,
+  });
+
+  console.log(allProducts, filteredProduct);
 
   if (isLoadingAll)
     return (
@@ -26,21 +34,27 @@ export default function ProductsPage({ searchParams }) {
         <Loader h={70} w={70} />
       </main>
     );
+
+  const productsToShow =
+    searchParams && Object.keys(searchParams).length > 0
+      ? filteredProduct
+      : allProducts;
+
   return (
     <main>
       <div className="flex items-center gap-2 w-full mb-[20px]">
         <FilterProducts />
         <SearchProducts />
       </div>
-      {allProducts.products.length !== 0 ? (
+      {productsToShow?.products.length !== 0 ? (
         <>
           <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[15px]">
-            {allProducts.products.map((product) => (
+            {productsToShow?.products.map((product) => (
               <ProductCard key={product._id} {...product} />
             ))}
           </section>
           <Pagination
-            totalPages={allProducts?.totalPages}
+            totalPages={productsToShow?.totalPages}
             searchParams={searchParams}
           />
         </>
