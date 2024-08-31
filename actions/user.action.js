ffda4ed7b64a04902d58fbc245167a15cb41553c
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-08-31 18:19:01 +0330

    Writing actions related to updating user information.

--- actions/user.action.js
+++ actions/user.action.js
@@ -41,3 +41,76 @@ export const getUser = async () => {
     };
   }
 };
+
+export const uppdateUser = async (req) => {
+  try {
+    await connectDB();
+
+    const session = getServerSession();
+
+    if (!session)
+      return {
+        message: MESSAGES.unAuthorized,
+        status: MESSAGES.failed,
+        code: STATUS_CODES.unAuthorized,
+      };
+
+    const { username, displayName, phoneNumber, address, gender } = req;
+    const user = await UserSorme.findById(session.userId);
+
+    if (!user)
+      return {
+        message: MESSAGES.userNotFound,
+        status: MESSAGES.failed,
+        code: STATUS_CODES.not_found,
+      };
+
+    if (username !== session.username) {
+      const checkedUsename = await UserSorme.findOne({ username });
+      if (checkedUsename) {
+        return {
+          message: MESSAGES.user_exist,
+          status: MESSAGES.failed,
+          code: STATUS_CODES.exist,
+        };
+      }
+    }
+
+    user.username = username;
+    user.displayName = displayName;
+    user.phoneNumber = phoneNumber;
+    user.address = address;
+    user.gender = gender;
+
+    await user.save();
+
+    const accessToken = sign(
+      { username, role: user.role, gender: user.gender, userId: user.id },
+      SECRET_KEY,
+      {
+        expiresIn: SESSION_EXPIRATION,
+      }
+    );
+
+    cookies().set("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(Date.now() + SESSION_EXPIRATION),
+      sameSite: "lax",
+      path: "/",
+    });
+
+    return {
+      user,
+      message: MESSAGES.updateProfile,
+      status: MESSAGES.success,
+      status: STATUS_CODES.updated,
+    };
+  } catch (error) {
+    return {
+      message: MESSAGES.server,
+      status: MESSAGES.failed,
+      code: STATUS_CODES.server,
+    };
+  }
+};
