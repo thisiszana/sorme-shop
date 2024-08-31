17cbe903f93f6c850687cade6cf8515226421146
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-08-31 20:19:00 +0330

    Write queriesKey file for queryKey keys.

---
+++ services/queriesKey.js
@@ -0,0 +1,9 @@
+const QUERY_KEY = {
+  user_session: "user-session",
+  user_data: "user-data",
+  user_cart: "user-cart",
+  allProducts: "allProducts",
+  filteredProducts: "filteredProducts",
+};
+
+export { QUERY_KEY };
