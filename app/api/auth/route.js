26a27dc95a54b7dfba7c4de0a76e022c5bbfa00d
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-09-02 09:43:54 +0330

    Create api route user session.

---
+++ app/api/auth/route.js
@@ -0,0 +1,21 @@
+import { getServerSession } from "@/utils/session";
+
+export async function GET() {
+  try {
+    const session = getServerSession();
+
+    if (session) {
+      return NextResponse.json(
+        { session, status: "authorized" },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { session: null, status: "un-authorized" },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ message: "Server Error!" }, { status: 500 });
+  }
+}
