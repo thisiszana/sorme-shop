979b7ec3b618461e8a72714de8f20cf202406cf6
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-09-09 12:16:06 +0330

    Implementing the orders page from the user's profile and showing the user's order list.

--- components/pages/orders/OrdersPage.jsx
+++ components/pages/orders/OrdersPage.jsx
@@ -1,18 +1,18 @@
-import { getUser } from "@/actions/user.action";
+import { getUserOrders } from "@/actions/order.action";
 
 import ProfilePageHeader from "@/components/shared/ProfilePageHeader";
 import MyOrders from "./ui/MyOrders";
 
 export default async function OrdersPage() {
-  const data = await getUser();
-  console.log("order page data", data);
+  const data = await getUserOrders();
+
   if (data.code !== 200) {
     return <p>Error!</p>;
   }
   return (
     <>
       <ProfilePageHeader title="My Orders" />
-      {/* <MyOrders orders={JSON.parse(JSON.stringify(data?.user?.orders))} /> */}
+      <MyOrders orders={JSON.parse(JSON.stringify(data.orders.orders))} />
     </>
   );
 }
